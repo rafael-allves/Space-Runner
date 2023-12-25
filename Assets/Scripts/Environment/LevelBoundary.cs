dcb16410fd51e14a4f3d3cfb3b78115e238028b2
rafael <rafa.viana.moc@gmail.com>
2023-12-25 13:58:26 -0300

    Horizontal player move goodly setted !

--- Assets/Scripts/Environment/LevelBoundary.cs
+++ Assets/Scripts/Environment/LevelBoundary.cs
@@ -1,19 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Environment
 {
     public class LevelBoundary : MonoBehaviour
     {
+        public static readonly float EdgeBorder = 1.5f;
+        
+        private float _internalLeft;
+        private float _internalRight;
+        
         void Start()
         {
-
-        }
-
-        void Update()
-        {
-
+            _internalLeft = -EdgeBorder;
+            _internalRight = EdgeBorder;
         }
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,27 +1,91 @@
 using UnityEngine;
 
+using Environment; 
+
 namespace Player
 {
     /**
-     * this class has the responsability to manage the player Movements
+     * Enum to represent movement directions.
+     */
+    public enum MovementDirection
+    {
+        Idle,
+        Left,
+        Right,
+        Top,
+        Bottom
+    }
+
+    /**
+     * This class manages the player movements.
      */
     public class PlayerMove : MonoBehaviour
     {
         private StateManager _player;
-        
-        private void Start()
+        private Transform _transform;
+        private readonly float _xAxisSpeed = 4f;
+        private MovementDirection _moveDirection = MovementDirection.Idle;
+
+        // Define the points the player can move to
+        private readonly float[] _movePoints = new float[] {-LevelBoundary.EdgeBorder, 0f, LevelBoundary.EdgeBorder}; // Example positions
+        private int _currentPointIndex = 1; // Starting at the middle point (index 1 in this case)
+
+        private void Awake()
         {
             _player = GetComponent<StateManager>();
+            _transform = transform;
         }
-    
+
         private void Update()
         {
+            CheckForInput();
             MovePlayer();
+        }
+
+        private void CheckForInput()
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (_currentPointIndex < _movePoints.Length - 1)
+                {
+                    _currentPointIndex++;
+                    _moveDirection = MovementDirection.Right;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (_currentPointIndex > 0)
+                {
+                    _currentPointIndex--;
+                    _moveDirection = MovementDirection.Left;
+                }
+            }
         }
 
         private void MovePlayer()
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * _player.playerSpeed, Space.World);
+            _transform.Translate(Vector3.forward * Time.deltaTime * _player.playerSpeed, Space.World);
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            if (_moveDirection != MovementDirection.Idle)
+            {
+                Vector3 newPosition = _transform.position;
+                MoveTowardsDirection(ref newPosition, _movePoints[_currentPointIndex]);
+                _transform.position = newPosition;
+            }
+        }
+
+        private void MoveTowardsDirection(ref Vector3 currentPosition, float targetX)
+        {
+            currentPosition.x = Mathf.MoveTowards(currentPosition.x, targetX, _xAxisSpeed * Time.deltaTime);
+            
+            float tolerance = 0.001f;
+            
+            if (Mathf.Abs(currentPosition.x - targetX) < tolerance)
+                _moveDirection = MovementDirection.Idle;
         }
     }
 }
